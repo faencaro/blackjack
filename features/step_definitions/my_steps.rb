7d89c9b08c54ce712e97feee4dff6fa6ab49795f
kleer <kleer@kleer-csd-box>
2017-07-19 13:58:02 -0500

    .

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -5,3 +5,8 @@ end
 Then(/^al hacer clic en Jugar debe abrir pagina de juego$/) do
   click_button("Jugar")
 end
+
+Then(/^se deben ver dos numeros aleatorios entre el (\d+) y el (\d+)$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
